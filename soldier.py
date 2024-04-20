b75357e28a4b9e07b7484d358d7802f471131b61
ijoanet <joanramosm@gmail.com>
2024-04-20 13:29:08 +0200

    split vikingsClasses.py

---
+++ soldier.py
@@ -0,0 +1,11 @@
+import random
+
+class Soldier:
+    def __init__(self, health, strength):
+        print("Soldier created")
+
+    def attack(self):
+        print("Soldier attacked")
+
+    def receiveDamage(self, damage):
+        print("Soldier received damage")
--- vikingsClasses.py
+++ vikingsClasses.py
@@ -1,83 +1,18 @@
 import random
 
+
 # Soldier
-
-
-class Soldier:
-    def __init__(self, health, strength):
-        # your code here
-    
-    def attack(self):
-        # your code here
-
-    def receiveDamage(self, damage):
-        # your code here
-    
+from soldier import Soldier
 
 # Viking
-
-class Viking(Soldier):
-    def __init__(self, name, health, strength):
-        # your code here
-
-    def battleCry(self):
-        # your code here
-
-    def receiveDamage(self, damage):
-        # your code here
+from viking import Viking
 
 # Saxon
-
-class Saxon(Soldier):
-    def __init__(self, health, strength):
-        # your code here
-
-    def receiveDamage(self, damage):
-        # your code here
+from saxon import Saxon
 
 # Davicente
-
-class War():
-    def __init__(self):
-        # your code here
-
-    def addViking(self, viking):
-        # your code here
-    
-    def addSaxon(self, saxon):
-        # your code here
-    
-    def vikingAttack(self):
-        # your code here
-    
-    def saxonAttack(self):
-        # your code here
-
-    def showStatus(self):
-        # your code here
-    pass
+from war import War
 
 #yop
-class War2:
-
-    def __init__(self):
-        # your code here
-
-    def addViking(self, Viking):
-        # your code here
-    
-    def addSaxon(self, Saxon):
-        # your code here
-    
-    def vikingAttack(self):
-        # your code here
-
-    def saxonAttack(self):
-        # your code here
-
-    def showStatus(self):
-        # your code here
-
-    pass
-
+from war2 import War2
 
