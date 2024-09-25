646f86870cafcf80b53c42775bfd89fdbacd653e
Jose Antonio Vargas Garcia <tonacovargas@hotmail.com>
2024-09-25 07:28:17 +0200

    update 25/09

---
+++ .ipynb_checkpoints/vikingsClasses-checkpoint.py
@@ -0,0 +1,88 @@
+import random
+
+# Soldier
+
+
+class Soldier:
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+    
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.health -= damage
+    
+
+# Viking
+
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        self.name = name
+        self.health = health
+        self.strength = strength
+
+    def battleCry(self):
+        # your code here
+
+    def receiveDamage(self, damage):
+        # your code here
+
+
+'''
+# Saxon
+
+class Saxon(Soldier):
+    def __init__(self, health, strength):
+        # your code here
+
+    def receiveDamage(self, damage):
+        # your code here
+
+# Davicente
+
+class War():
+    def __init__(self):
+        # your code here
+
+    def addViking(self, viking):
+        # your code here
+    
+    def addSaxon(self, saxon):
+        # your code here
+    
+    def vikingAttack(self):
+        # your code here
+    
+    def saxonAttack(self):
+        # your code here
+
+    def showStatus(self):
+        # your code here
+    pass
+
+#yop
+class War2:
+
+    def __init__(self):
+        # your code here
+
+    def addViking(self, Viking):
+        # your code here
+    
+    def addSaxon(self, Saxon):
+        # your code here
+    
+    def vikingAttack(self):
+        # your code here
+
+    def saxonAttack(self):
+        # your code here
+
+    def showStatus(self):
+        # your code here
+
+    pass
+
+'''
--- vikingsClasses.py
+++ vikingsClasses.py
@@ -5,58 +5,91 @@ import random
 
 class Soldier:
     def __init__(self, health, strength):
-        # your code here
+        self.health = health
+        self.strength = strength
     
     def attack(self):
-        # your code here
+        return self.strength
 
     def receiveDamage(self, damage):
-        # your code here
+        self.health -= damage
     
 
 # Viking
 
 class Viking(Soldier):
     def __init__(self, name, health, strength):
-        # your code here
+        super().__init__(health, strength)
+        self.name = name
+
+    def attack(self):
+        return self.strength
 
     def battleCry(self):
-        # your code here
+        return 'Odin Owns You All!'
 
     def receiveDamage(self, damage):
-        # your code here
+        self.health -= damage
+        if self.health > 0:
+            #return self.name + ' has received 70 points of damage'
+            #return self.name + ' has received 50 points of damage'
+            return f'{self.name} has received {damage} points of damage'
+        else:
+            #return self.name + ' has died in act of combat'
+            return f'{self.name} has died in act of combat'
+            #self.name + ' has died in act of combat'
+
+
 
 # Saxon
 
 class Saxon(Soldier):
     def __init__(self, health, strength):
-        # your code here
+        super().__init__(health, strength)
 
     def receiveDamage(self, damage):
-        # your code here
+        self.health -= damage
+        if self.health > 0:
+            #return f"Un 'Saxon' ha recibido {damage} puntos de daño."
+            return f'A Saxon has received {damage} points of damage'
+        else:
+            return f'A Saxon has died in combat'
+
+
+
 
 # Davicente
 
 class War():
     def __init__(self):
-        # your code here
+        self.vikingArmy = []
+        self.saxonArmy = []
 
     def addViking(self, viking):
-        # your code here
+        self.vikingsArmy.append(viking)
     
     def addSaxon(self, saxon):
-        # your code here
+        self.saxonArmy.append(saxon)
     
     def vikingAttack(self):
-        # your code here
+        saxon.health -= viking.strength
+        if saxon.health <= 0:
+            saxonArmy.pop()
+        return f'{receiveDamage()} {Saxon} {strength} {Viking}'
     
     def saxonAttack(self):
-        # your code here
+        return f'{receiveDamage()} {Viking} {strength} {Saxon}'
 
     def showStatus(self):
-        # your code here
-    pass
+        if len(saxonArmy) == 0:
+            return 'Vikings have won the war of the century!'
+        if len(vikingArmy) == 0:
+            return 'Saxons have fought for their lives and survive another day...'
+        else:
+            return 'Vikings and Saxons are still in the thick of battle.'
 
+
+'''
 #yop
 class War2:
 
@@ -80,4 +113,4 @@ class War2:
 
     pass
 
-
+'''
